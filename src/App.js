e585728416aa10bb6dc7cfb0b79f8b8dfcffd185
Your Name <samanbatool08@gmail.com>
2020-10-17 22:12:43 -0400

    select dropdown from material ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import {
+  MenuItem, 
+  FormControl,
+  Select, 
+  Card,
+  CardContent,
+} from '@material-ui/core';
 import './App.css';
 
 function App() {
   return (
     <div className="app">
-      <h1>building covid19 tracker!!</h1>
+      <h1>COVID-19 TRACKER</h1>
+      <FormControl className='app__dropdown'>
+        <Select 
+          variant='outlined'
+          value='abc'
+          >
+          <MenuItem value='worldwide'>Worldwide</MenuItem>
+          <MenuItem value='worldwide'>Worldwide</MenuItem>
+          <MenuItem value='worldwide'>Worldwide</MenuItem>
+          <MenuItem value='worldwide'>Worldwide</MenuItem>
 
+        </Select>
+      </FormControl>
       {/* Header */}
       {/* Ttitle + Select Input dropdown field */}
 
