f2ba72159e58e361a11fc10a1dfddee36257442c
Your Name <samanbatool08@gmail.com>
2020-10-17 22:37:11 -0400

    fetching country data

--- src/App.js
+++ src/App.js
@@ -13,6 +13,22 @@ function App() {
     "USA", "UK", "INDIA"
   ]);
 
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch('https://disease.sh/v3​/covid-19​/countries')
+      .then((response) => response.json())
+      .then((data) => {
+        const countries = data.map((country) => (
+            {
+              name: country.country, // United States, United Kingdom
+              value: country.countryInfo.iso2 // UK, USA, FR
+
+            }
+        ))
+      })
+    }
+  }, [])
+
   return (
     <div className="app">
       <div className='app__header'>
