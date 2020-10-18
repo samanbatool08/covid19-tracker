216ff8ea2082344520bd27557f82a211314043c4
Your Name <samanbatool08@gmail.com>
2020-10-17 22:26:37 -0400

    hooks to retrieve country name

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   MenuItem, 
   FormControl,
@@ -9,16 +9,29 @@ import {
 import './App.css';
 
 function App() {
+  const [countries, setCountries] = useState([
+    "USA", "UK", "INDIA"
+  ]);
+
   return (
     <div className="app">
       <div className='app__header'>
         <h1>COVID-19 TRACKER</h1>
         <FormControl className='app__dropdown'>
           <Select variant='outlined' value='abc'>
-            <MenuItem value='worldwide'>Worldwide</MenuItem>
+            {/* Loop through list of countries and show a dropdown list of country options */}
+           
+           {
+            countries.map(country => (
+              <MenuItem value={country}>{country}</MenuItem>
+            ))
+
+           }
+           
+            {/* <MenuItem value='worldwide'>Worldwide</MenuItem>
             <MenuItem value='worldwide'>Option 2</MenuItem>
             <MenuItem value='worldwide'>Option 3</MenuItem>
-            <MenuItem value='worldwide'>Option 4</MenuItem>
+            <MenuItem value='worldwide'>Option 4</MenuItem> */}
           </Select>
         </FormControl>
       </div>
