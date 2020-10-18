903938026d4b4c291bcd5496be383a6b04fe1529
Your Name <samanbatool08@gmail.com>
2020-10-17 22:22:10 -0400

    header flex

--- src/App.js
+++ src/App.js
@@ -11,19 +11,17 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-      <h1>COVID-19 TRACKER</h1>
-      <FormControl className='app__dropdown'>
-        <Select 
-          variant='outlined'
-          value='abc'
-          >
-          <MenuItem value='worldwide'>Worldwide</MenuItem>
-          <MenuItem value='worldwide'>Worldwide</MenuItem>
-          <MenuItem value='worldwide'>Worldwide</MenuItem>
-          <MenuItem value='worldwide'>Worldwide</MenuItem>
-
-        </Select>
-      </FormControl>
+      <div className='app__header'>
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl className='app__dropdown'>
+          <Select variant='outlined' value='abc'>
+            <MenuItem value='worldwide'>Worldwide</MenuItem>
+            <MenuItem value='worldwide'>Option 2</MenuItem>
+            <MenuItem value='worldwide'>Option 3</MenuItem>
+            <MenuItem value='worldwide'>Option 4</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
       {/* Header */}
       {/* Ttitle + Select Input dropdown field */}
 
