8547f9fabbccb22325aaf4767a0973df9fc22d49
Your Name <samanbatool08@gmail.com>
2020-10-22 23:24:05 -0400

    line graph

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Line } from 'react-chartjs-2';
 
 function LineGraph() {
+    const [data, setData] = useState({});
+
+    // endpoint: https://disease.sh/v3/covid-19/historical/all?lastdays=120
+
+    useEffect(() => {
+        fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
+        .then(response => response.json())
+        .then(data => {
+            console.log('line', data)
+        })
+    }, [])
+
     return (
         <div>
-            
+            <h1>I am a graph</h1>
+            {/* <Line 
+
+            /> */}
         </div>
     )
 }
