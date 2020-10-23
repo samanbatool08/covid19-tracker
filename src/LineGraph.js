ea7eeaefeb6123c303152f745ec6cf095cfcbde6
Your Name <samanbatool08@gmail.com>
2020-10-23 14:27:44 -0400

    cases error

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -6,36 +6,39 @@ function LineGraph() {
 
     // endpoint: https://disease.sh/v3/covid-19/historical/all?lastdays=120
 
+ 
+    const buildChartData = (data, casesType = "cases") => {
+        const chartData = [];
+        let lastDataPoint;
+        data[casesType].forEach((date) => {
+            if (lastDataPoint) {
+                const newDataPoint = {
+                    x: date, 
+                    y: data[casesType][date] - lastDataPoint,
+                };
+                chartData.push(newDataPoint)
+            }
+            lastDataPoint = data[casesType][date]
+        })
+        return chartData; 
+    };
+
     useEffect(() => {
         fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
         .then(response => response.json())
         .then(data => {
             console.log('line', data)
+            const chartData = buildChartData(data)
+            setData(chartData)
         })
     }, [])
-
-    const buildChartData = (data, casesType='cases') => {
-        const chartData = [];
-        let lastDataPoint;
-
-        data[casesType].cases.forEach(date => {
-            if(lastDataPoint){
-                const newDataPoint = {
-                    x: date, 
-                    y: data[casesType][date] - lastDataPoint
-                }
-                chartData.push(newDataPoint)
-            }
-            lastDataPoint = data['cases']
-        })
-        return chartData; 
-    }
 
     return (
         <div>
             <h1>I am a graph</h1>
             {/* <Line 
-
+                data
+                options
             /> */}
         </div>
     )
